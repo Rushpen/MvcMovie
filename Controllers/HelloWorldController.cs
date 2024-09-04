a5788f63c6ada0a5e65e7ad80928f06ef2bd9e12
G_Rus <G_Rus@RUSHAN>
2024-09-04 20:18:15 +0300

    Done part3(Create view)

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,21 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
-using System.Xml.Linq;
 
 namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // 
-    // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
-    // 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
