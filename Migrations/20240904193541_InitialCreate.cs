b8551991dfd081862d559767d9de74b5c4c5ed19
G_Rus <G_Rus@RUSHAN>
2024-09-04 23:02:31 +0300

    Part 4 Done.

--- Migrations/20240904193541_InitialCreate.cs
+++ Migrations/20240904193541_InitialCreate.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace MvcMovie.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -28,7 +26,6 @@ namespace MvcMovie.Migrations
                 });
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
